623ed9c31da335bb367778c9d92d65775eebb7b5
greeeg <gregoire.mielle@pennylane.com>
2024-03-17 10:51:24 +0100

    feat: ability to log SQL queries

--- packages/api/src/utils/prisma.service.ts
+++ packages/api/src/utils/prisma.service.ts
@@ -1,8 +1,37 @@
 import { Injectable, OnModuleInit } from '@nestjs/common'
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
+import { MyLogger } from './logger'
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, 'query'>
+  implements OnModuleInit
+{
+  constructor() {
+    super({
+      log:
+        process.env.DATABASE_LOG_QUERIES === 'true'
+          ? [
+              {
+                emit: 'event',
+                level: 'query',
+              },
+            ]
+          : [],
+    })
+
+    const logger = new MyLogger()
+
+    this.$on('query', ({ query, params, duration }) => {
+      logger.log('Database query', {
+        service: 'database',
+        query,
+        params,
+        duration,
+      })
+    })
+  }
+
   async onModuleInit() {
     await this.$connect()
   }
