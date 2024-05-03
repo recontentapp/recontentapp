e2af4271d0ed11242cc0ca354c755b206ad28e40
greeeg <gregoire.mielle@pennylane.com>
2024-05-03 09:07:17 +0200

    feat(file-formats): add missing specs

--- packages/file-formats/src/formats/csv/parse.spec.ts
+++ packages/file-formats/src/formats/csv/parse.spec.ts
@@ -1,3 +1,13 @@
+import { testDict } from '../../testHelpers'
+import { parseCSV } from './parse'
+
 describe('csv', () => {
-  it.todo('returns expected output')
+  it('returns expected output', async () => {
+    const testData = `title,Hello World
+dashboard.title,Welcome to your dashboard
+dashboard.description,Here you can find all the information you need to get started`
+
+    const result = await parseCSV(Buffer.from(testData))
+    expect(result).toEqual(testDict)
+  })
 })
--- packages/file-formats/src/formats/yaml/parse.spec.ts
+++ packages/file-formats/src/formats/yaml/parse.spec.ts
@@ -1,3 +1,14 @@
+import { testDict } from '../../testHelpers'
+import { parseYAML } from './parse'
+
 describe('yaml', () => {
-  it.todo('returns expected output')
+  it('returns expected output', async () => {
+    const testData = `title: Hello World
+dashboard:
+  title: Welcome to your dashboard
+  description: Here you can find all the information you need to get started`
+
+    const result = await parseYAML(Buffer.from(testData))
+    expect(result).toEqual(testDict)
+  })
 })
