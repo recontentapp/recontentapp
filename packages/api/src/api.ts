0b4a92df9cb437479bdda18272d8bfd0204bfd08
greeeg <gregoire.mielle@pennylane.com>
2024-05-04 15:15:12 +0200

    fix(api): update cors config to limit preflight requests & add helmet for security

--- packages/api/src/api.ts
+++ packages/api/src/api.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import * as bodyParser from 'body-parser'
+import helmet from 'helmet'
 
 import { ApiModule } from './adapters/api/api.module'
 import { MyLogger } from './utils/logger'
@@ -11,13 +12,18 @@ const BODY_PARSER_LIMIT = '10MB'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(ApiModule, {
-    cors: true,
     logger: new MyLogger(),
   })
   app.disable('x-powered-by')
   if (process.env.TRUST_PROXY === 'true') {
     app.enable('trust proxy')
   }
+  app.enableCors({
+    maxAge: 600,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  })
+  app.use(helmet())
   app.use(bodyParser.json({ limit: BODY_PARSER_LIMIT }))
   app.use(
     bodyParser.urlencoded({
