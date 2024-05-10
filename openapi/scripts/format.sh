2fe8cb7deb39c4a9b682b7212f5655002574dabe
greeeg <gregoire.mielle@pennylane.com>
2024-05-10 18:48:08 +0200

    fix: fix docker build

--- openapi/scripts/format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-openapi-format ./openapi/private-api.yml -o ./openapi/private-api.yml
-openapi-format ./openapi/public-api.yml -o ./openapi/public-api.yml
-openapi-format ./openapi/figma-plugin-api.yml -o ./openapi/figma-plugin-api.yml
