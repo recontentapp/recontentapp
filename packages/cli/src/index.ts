e9fc98e968aca0ca598ca169f96a98f732a7f2d8
greeeg <gregoire.mielle@pennylane.com>
2024-08-09 14:18:12 +0200

    v0.6.0

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -7,7 +7,7 @@ import { API_KEY_ENV, getEnvironment } from './utils/environment'
 program
   .name('recontentapp')
   .description('Ship localized content faster without loosing engineers time')
-  .version('0.5.0')
+  .version('0.6.0')
   .hook('preAction', (command: Command) => {
     const { apiKey } = getEnvironment()
     if (!apiKey) {
